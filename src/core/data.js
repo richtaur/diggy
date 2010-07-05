f9665ac13eb0ff9c571e0b0cb2ac690b4a4f89f5
richtaur <hackett@raptr.com>
2010-07-05 10:25:31 -0700

    last stuff to be added, hopefully

--- src/core/data.js
+++ src/core/data.js
@@ -7,69 +7,59 @@ DGE.Data = new DGE.Object({
 	id : 'DGE.Data'
 });
 
-DGE.Data.get = function(key) {
+if (typeof(localStorage != 'undefined')) {
 
-	if (typeof(localStorage) == 'undefined') {
-		// TODO
-		var value = null;
-	} else {
+	DGE.Data.get = function(key) {
+
 		var value = localStorage[key];
-	}
 
-	if (value === 'true') {
-		return true;
-	} else if (value === 'false') {
-		return false;
-	}
-
-	return value;
-
-};
-
-DGE.Data.set = function(key, value) {
-
-	if (typeof(key) == 'object') {
-
-		for (var k in key) {
-			arguments.callee.apply(DGE.Data, [k, key[k]]);
+		if (value === 'true') {
+			return true;
+		} else if (value === 'false') {
+			return false;
 		}
 
-	} else {
+		return value;
 
-		var previous = DGE.Data.data[key];
-		this.data[key] = value;
+	};
 
-		if (typeof(localStorage) == 'undefined') {
-			// TODO
+	DGE.Data.set = function(key, value) {
+
+		if (typeof(key) == 'object') {
+
+			for (var k in key) {
+				arguments.callee.apply(DGE.Data, [k, key[k]]);
+			}
+
 		} else {
+
+			var previous = DGE.Data.data[key];
+			this.data[key] = value;
+
 			localStorage[key] = value;
+
+			if (value !== previous) {
+				DGE.Data.fire(DGE.sprintf('change:%s', key), value);
+			}
+
+			DGE.Data.fire(DGE.sprintf('set:%s', key), value);
+
 		}
 
-		if (value !== previous) {
-			DGE.Data.fire(DGE.sprintf('change:%s', key), value);
-		}
+	};
 
-		DGE.Data.fire(DGE.sprintf('set:%s', key), value);
+	/**
+	 * Empties the persistent memory.
+	 * @return {Object} this (for chaining).
+	 * @method add
+	 */
+	DGE.Data.empty = function() {
 
-	}
-
-};
-
-/**
- * Empties the persistent memory.
- * @return {Object} this (for chaining).
- * @method add
- */
-DGE.Data.empty = function() {
-
-	DGE.Data.data = {};
-
-	if (typeof(localStorage) == 'undefined') {
-		// TODO
-	} else {
+		DGE.Data.data = {};
 		localStorage = {};
-	}
 
-	return DGE.Data;
+		return DGE.Data;
 
-};
+	};
+
+}
